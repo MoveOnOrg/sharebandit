27eaae730a3f2d136892523ee922cc1e8d82912c
Schuyler Duveen <sky@skyb.us>
2016-02-25 12:14:16 -0500

    stub for redirect view

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ app.get('/',
 	}
 );
 
+app.get('/r/:domain*',
+  function (req, res) {
+    //if (! (req.params.domain in config.domain_whitelist))
+    //  res.404!
+    res.vary('User-Agent')
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+
+    var proto = 'http' || config.domain_whitelist[res.params.domain].proto;
+
+
+    //https://developers.facebook.com/docs/sharing/webmasters/crawler
+    if (/facebookexternalhit|Facebot/.test(req.get('User-Agent'))) {
+      res.render('shareheaders', {});
+    } else {
+      res.redirect(proto + res.params.domain + decodeURIComponent(res.params[0] || ''));
+    }
+  }
+);
+
 // Launch server.
 var server = app.listen(8080, function () {
   var port = server.address().port;
