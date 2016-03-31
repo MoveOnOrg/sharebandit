2b71f3d1b78354037cb2db11b726d2279e41dfb4
Schuyler Duveen <sky@skyb.us>
2016-03-31 18:32:52 -0400

    remove "moveon" from googleAuth and make it generic WARNING: you will need to change your config.json

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var config = require('./config.json');
-var db = config.postgresql;
+var configFile = require('./config.json');
 var _ = require('lodash');
 var express = require('express');
 var app = express();
@@ -8,49 +7,74 @@ var bodyParser = require('body-parser');
 var google = require('googleapis');
 var url = require('url');
 var OAuth2 = google.auth.OAuth2;
-var oauth2Client = new OAuth2(
-  config.oauth.clientId,
-  config.oauth.clientSecret,
-  config.baseUrl + '/auth/google/callback'
-);
-var moveonAuth = require('./node_modules/moveon-auth');
+var googleAuth = require('./node_modules/moveon-auth');
 var swig  = require('swig');
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(db.database, db.user, db.pass, {
-  dialect: "postgres",
-  port: 5432,
-});
-var schema = require('./schema.js')(sequelize);
 
-// Configure Express app
-app.use(session({
-	secret: config.sessionSecret,
-  resave: true,
-  saveUninitialized: false
-}));
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+var server
 
-
-var public_views = require('./public.js')(app, schema, sequelize);
-
-var adminauth = moveonAuth({'oauth2Client': oauth2Client, 'app': app, 'domain': 'moveon.org'}).confirm;
-if (/\/\/localhost/.test(config.baseUrl) && config.develMode) {
-  adminauth = function(req,res,next) {next();};
+var shutown = function() {
+  server.close();
 }
-var admin_views = require('./admin.js')(app, schema, sequelize, adminauth);
-
-app.get('/',
-  adminauth,  function (req, res) {
-    res.redirect('/admin/');
-  }
-);
 
 // Launch server.
-var server = app.listen(config.port, function () {
-  var port = server.address().port;
-  console.log('App listening at %s:%s', config.baseUrl, port);
-});
+var boot = function(config) {
+  if (!config) {
+    config = configFile;
+  }
+
+  // Configure Express app
+  app.use(session({
+    secret: config.sessionSecret,
+    resave: true,
+    saveUninitialized: false
+  }));
+  app.use(express.static(__dirname + '/public'));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.engine('html', swig.renderFile);
+  app.set('view engine', 'html');
+  app.set('views', __dirname + '/views');
+
+
+  //MODELS
+  var db = config.db;
+  var sequelize = new Sequelize(db.database, db.user, db.pass, db);
+  var schema = require('./schema.js')(sequelize);
+
+  //VIEWS
+  var public_views = require('./public.js')(app, schema, sequelize);
+
+  var adminauth;
+  if (/\/\/localhost/.test(config.baseUrl) && config.develMode) {
+    adminauth = function(req,res,next) {next();};
+  } else {
+    var oauth2Client = new OAuth2(
+      config.oauth.clientId,
+      config.oauth.clientSecret,
+      config.baseUrl + '/auth/google/callback'
+    );
+    adminauth = (googleAuth({'oauth2Client': oauth2Client,
+                             'app': app,
+                             'whitelist': config.oauthAllowedUsers
+                            }).confirm);
+  }
+  var admin_views = require('./admin.js')(app, schema, sequelize, adminauth);
+  
+  app.get('/',
+          adminauth,  function (req, res) {
+            res.redirect('/admin/');
+          }
+         );
+  server = app.listen(config.port, function () {
+    var port = server.address().port;
+    console.log('App listening at %s:%s', config.baseUrl, port);
+  });
+}
+
+if (require.main === module) {
+  boot(configFile);
+} else {
+  exports.server = server;
+  exports.boot = boot;
+  exports.shutdown = shutdown;
+}
