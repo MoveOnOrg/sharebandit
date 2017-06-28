db0b7ab588111ce3231dc11f8eb1ad6842c5247f
Ann Lewis <the.annlewis@gmail.com>
2017-06-28 16:32:15 -0700

    temporary debug logging

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+console.log("******** index 1")
 if (process.env.CONFIG) {
+  console.log("******** getting config from " + process.env.CONFIG);
+
   var configFile = JSON.parse(process.env.CONFIG);
 } else {
+  console.log("******** getting config from ./config/config.json");
   var configFile = require('./config/config.json');
+  //console.log("******** configFile: " + configFile); 
 }
 
+//var configFile;
+console.log("******** index 2");
 var _ = require('lodash');
 var express = require('express');
 var app = express();
@@ -16,19 +23,27 @@ var googleAuth = require('./lib/google-auth');
 var swig  = require('swig');
 var Sequelize = require('sequelize');
 
+
+console.log("******** index 3")
+
 var dbconn = {};
 var server;
 
 var shutdown = function() {
   server.close();
 }
+console.log("******** index 4")
 
 // Launch server.
 var boot = function(config) {
+  console.log("******** index boot start")
+
   if (!config) {
     console.log('loading config from config/config.json');
     config = configFile;
   }
+
+  console.log("******** Configure Express app")
 
   // Configure Express app
   app.use(session({
@@ -43,6 +58,7 @@ var boot = function(config) {
 
   var modules = [];
 
+  console.log("******** load modules")
   //MODULES
   if (config.extensionModules) {
     modules = config.extensionModules.map(function(m) {
@@ -55,6 +71,7 @@ var boot = function(config) {
       }
     });
   }
+  console.log("******** connect to database")
 
   //MODELS
   var db = config.db;
@@ -63,8 +80,10 @@ var boot = function(config) {
   dbconn.schema = schema;
 
   //VIEWS
+  console.log("******** wire up views")
   var public_views = require('./public.js')(app, schema, sequelize, config);
 
+  console.log("******** wire up admin")
   var adminauth;
   if (/\/\/localhost/.test(config.baseUrl) && config.develMode) {
     adminauth = function(req,res,next) {next();};
@@ -74,15 +93,19 @@ var boot = function(config) {
       config.oauth.clientSecret,
       config.baseUrl + '/auth/google/callback'
     );
+    console.log("****** config.baseUrl " + config.baseUrl );
+
     adminauth = (googleAuth({'oauth2Client': oauth2Client,
                              'app': app,
                              'whitelist': config.oauthAllowedUsers
                             }).confirm);
+    console.log("***** after adminauth");
   }
   var view_dirs = [__dirname + '/views'];
   //MODULES
   // get the links that will be available in the admin
   //  -- all other views, the module should setup itself
+  console.log("******** wire up modules")
   var moduleLinks = modules.map(function(m) {
       var moduleResult = m(app, schema, sequelize, adminauth, config);
       view_dirs.push(moduleResult.viewDirectory);
@@ -90,20 +113,21 @@ var boot = function(config) {
   });
 
   app.set('views', view_dirs);
+  console.log("******** wire up sequelize")
 
   sequelize.authenticate();
   sequelize.sync();
 
   var admin_views = require('./admin.js')(app, schema, sequelize, adminauth, config, moduleLinks);
 
+  console.log("******** about to app.get")
   app.get('/',
           adminauth,  function (req, res) {
-            // res.redirect('/admin/');
-            console.log('Hello world');
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end('Hello World!');
+            console.log('INDEX PATH Hello world');
+            res.redirect(config.baseUrl + '/admin/');
           }
          );
+
   // server = app.listen(config.port, function () {
   //   var port = server.address().port;
   //   console.log('App listening at %s:%s', config.baseUrl, port);
@@ -120,3 +144,4 @@ if (require.main === module) {
 }
 
 module.exports = app
+boot(configFile);
