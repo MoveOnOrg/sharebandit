8f933aad3031edee4648f4722c365c5bc967e8e6
Sandra Chung <sandra@moveon.org>
2017-08-07 10:07:53 -0600

    linted bandit.js

--- bandit.js
+++ bandit.js
@@ -21,57 +21,57 @@ var bayesBandit = function(url, sequelize, successMetric) {
   //      but really only about ~5 per rbeta call, so it'll be pretty fast
   
   var query = ('SELECT trial, sum(case when Sharers.{{success_field}}_count > 0 then 1 else 0 end) AS success, count(key) AS trials'
-                 +' FROM Sharers'
-                 +' JOIN Metadata on (Metadata.id = Sharers.trial)'
-                 +' WHERE Metadata.url = ? GROUP BY trial')
-              .replace('{{success_field}}',
-                       ((successMetric == 'action') ? 'action' : 'success')
-                      );
+    +' FROM Sharers'
+    +' JOIN Metadata on (Metadata.id = Sharers.trial)'
+    +' WHERE Metadata.url = ? GROUP BY trial')
+    .replace('{{success_field}}',
+      ((successMetric == 'action') ? 'action' : 'success')
+    );
 
   return new Promise(function(resolve, reject) {
-      //TODO
-      // what is cacheable:
-      // * variant list:
-      //   * successes/trials
-      //   * in theory could generate a bunch at once and cache those
-      /*
-       cache_key_list = [ URLS ] (sorted by #hits, and then dropping dicts
-       cache_dict = {
-          <URL>: {
-            hits: 1, //this should decay each ...day?
-            variants: [
-               { // from db result
-                 trial: <var_id>,
-                 trials: <trial_count>,
-                 success: <trial_success>,
+    //TODO
+    // what is cacheable:
+    // * variant list:
+    //   * successes/trials
+    //   * in theory could generate a bunch at once and cache those
+    /*
+     cache_key_list = [ URLS ] (sorted by #hits, and then dropping dicts
+     cache_dict = {
+        <URL>: {
+          hits: 1, //this should decay each ...day?
+          variants: [
+             { // from db result
+               trial: <var_id>,
+               trials: <trial_count>,
+               success: <trial_success>,
 
-                 // added
-                 // we should cache related to trials count
-                 rbeta_cache: []
-               }
-            ]
-            
-          }
-       }
-      */
-      sequelize
-        .query(
-          query,
-          {
-            replacements: [url],
-            type: sequelize.QueryTypes.SELECT
-          }
-        )
-        .then(function(variants) {
-          return chooseFromVariants(variants, resolve, reject, 1);
-        });
-    });
+               // added
+               // we should cache related to trials count
+               rbeta_cache: []
+             }
+          ]
+          
+        }
+     }
+    */
+    sequelize
+      .query(
+        query,
+        {
+          replacements: [url],
+          type: sequelize.QueryTypes.SELECT
+        }
+      )
+      .then(function(variants) {
+        return chooseFromVariants(variants, resolve, reject, 1);
+      });
+  });
 };
 
 function chooseFromVariants(variants, resolve, reject, numResults) {
   if (!variants || variants.length == 0) {
     resolve(null);
-  };
+  }
   var totalSuccess = variants.reduce(function(total, variant) {
     return total + variant.success;
   }, 0);
@@ -87,10 +87,10 @@ function chooseFromVariants(variants, resolve, reject, numResults) {
   } else {
     var rbetas = variants.map(function(variant) {
       var results = PD.rbeta(
-          numResults || 1, //number of results wanted
-          (1*variant.success) + 1,
-          (1*variant.trials) - (1*variant.success) + 1
-        );
+        numResults || 1, //number of results wanted
+        (1*variant.success) + 1,
+        (1*variant.trials) - (1*variant.success) + 1
+      );
       return [
         results[0],
         variant.trial,
